6323dcd22700b4d53e34e0220ca61efa75d99a08
Muhammed Didin <mdidin80@gmail.com>
2021-04-28 23:56:00 +0300

    First request sended, response taken successfully

--- src/main/java/com/example/demo/api/PersonController.java
+++ src/main/java/com/example/demo/api/PersonController.java
@@ -3,8 +3,12 @@ package com.example.demo.api;
 import com.example.demo.model.Person;
 import com.example.demo.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequestMapping("api/v1/person")
 @RestController
 public class PersonController {
     private final PersonService personService;
@@ -14,7 +18,8 @@ public class PersonController {
         this.personService = personService;
     }
 
-    public void addPerson(Person person){
+    @PostMapping
+    public void addPerson(@RequestBody Person person){
         personService.addPerson(person);
     }
 }
